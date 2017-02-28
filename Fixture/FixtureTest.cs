26605723bc5730a4450e5186d971fe2f2071b063
Mihail Gromov <Mihail Gromov>
2017-02-28 15:38:14 +0000

    fitnesse

---
+++ Fixture/FixtureTest.cs
@@ -0,0 +1,21 @@
+﻿using Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixture
+{
+    public class FixtureTest : fit.ColumnFixture
+    {
+        public Customer c = new Customer();
+        public string gender;
+        public int age;
+
+        public float CalcPremium()
+        {
+            return c.CalcPremium(age, gender);
+        }
+    }
+}
